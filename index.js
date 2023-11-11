f40fd763b06a9b6ae189eef76c7f59595150cf9d
Lashawn Butler <lashawnbutler50@gmail.com>
2023-11-11 15:15:22 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+const cards = ["Guadalupe", "Ollie", "Aki"];
+function writeCards(cards) {
+    const messages = []
+    for (let i = 0; i < cards.length; i++) {
+        messages.push(`Thank you, ${cards[i]}, for the wonderful surprise gift!`)
+    }
+    return messages
+}
+function countDown() {
+    let countDown = 0
+    while (countDown < 11) {
+        console.log(countDown++)
+    }
+}
